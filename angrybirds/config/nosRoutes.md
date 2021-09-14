60668a689bb2ee34b9c4fce7a639b62703fe3e2f
Olivier <olivier@ogasphere.com>
2021-09-14 17:32:41 +0200

    affichage liste oiseaux et page detail

---
+++ angrybirds/config/nosRoutes.md
@@ -0,0 +1,13 @@
+# Nos routes
+
+## Liste des oiseaux
+
+| URL | Méthode HTTP | Controller | Méthode | Commentaire |
+|--|--|--|--|--|
+| / | GET | Bird | list | - |
+
+## Détail d'un oiseau
+
+| URL | Méthode HTTP | Controller | Méthode | Commentaire |
+|--|--|--|--|--|
+| /bird/{birdname} | GET | Bird | show | - |
--- angrybirds/src/Controller/BirdsController.php
+++ angrybirds/src/Controller/BirdsController.php
@@ -1,7 +1,39 @@
 <?php
 
 
-class BirdsConroller {
 
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class BirdsController extends AbstractController
+{
     
+
+    /**
+     * Undocumented function
+     *
+     * @Route("/", name="home")
+     */
+    public function list() : Response
+    { 
+        include './../public//files//data.php';
+        return $this->render('home.html.twig', ['birds' => $birds]);
+    }
+
+    /**
+     * @route("/bird/{id}", methods={"GET","HEAD"})", name="bird_content")
+     *
+     * @param  mixed $id
+     * @return void
+     */
+    public function show($id)
+    {
+        include './../public//files//data.php';
+        return $this->render('showbird.html.twig', ['birds' => $birds[$id]]); 
+    }
 }
