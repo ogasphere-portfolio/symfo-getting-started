a37c790205c2a1fcd2de765e080763686ce0d26e
Olivier <olivier@ogasphere.com>
2021-09-15 15:18:44 +0200

    fin de cours

--- angrybirds/src/Controller/BirdsController.php
+++ angrybirds/src/Controller/BirdsController.php
@@ -5,9 +5,11 @@
 
 namespace App\Controller;
 
+
 use App\Entity\BirdModel;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
@@ -25,16 +27,28 @@ class BirdsController extends AbstractController
         return $this->file(__DIR__ . '/../../public/files/angry_birds_2015_calendar.pdf');
     }
 
+    
+
     /**
      * Undocumented function
      *
      * @Route("/", name="home")
      */
-    public function list() : Response
+    public function list(SessionInterface $session) : Response
     { 
-        
         $birdModel = new BirdModel();
         $birds = $birdModel->getAll();
+        $lastBird = $session->get('last_bird');
+        // dump($birds);
+        /*
+        la méthode render cherche les templates à partir du dossier templates
+        */
+
+        return $this->render('home.html.twig', [
+            'birds' => $birds,
+            'last_bird' => $lastBird,
+        ]);
+       
         return $this->render('home.html.twig', ['birds' => $birds]);
     }
 
@@ -43,22 +57,24 @@ class BirdsController extends AbstractController
      * @param  mixed $id
      * @return void
      */
-    public function show($id)
+    public function show($id, SessionInterface $session) 
     {
         $birdModel = new BirdModel();
-        $birds = $birdModel->get($id);
+        $bird = $birdModel->get($id);
 
           // si l'utilisateur a saisi un identifiant invalide
         // on récupère une valeur nulle dans la variable $bird
         // et on affiche une 404
-        if ($birds === null) {
+        if ($bird === null) {
             // affiche une page 404 avec le message d'erreur fournit en argument
             throw $this->createNotFoundException('The oisal does not exist'); 
           
         }
-
-        return $this->render('showbird.html.twig', ['birds' => $birds]); 
+            // on ajoute l'oiseau en session
+            $session->set('last_bird', $bird);
+            return $this->render('showbird.html.twig', ['birds' => $bird]); 
     }
+
     /**
      * Undocumented function
      *
@@ -77,3 +93,5 @@ class BirdsController extends AbstractController
         ]);
     }
 }
+
+
